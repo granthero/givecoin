60aed736ab03e2673beb40807ae434fe631759f3
Dexaran <dexaran820@gmail.com>
2017-08-20 12:35:09 -0400

    Added max supply overflow checks

--- ICO.sol
+++ ICO.sol
@@ -12,22 +12,27 @@ contract ICO is ownable{
     uint256 public start_timestamp = now;
     uint256 public end_timestamp = now + 28 days;
     uint256 public GiveCoins_per_ETH = 30000; // This means that 300 GC per 1 ETH
+    uint256 public max_cap = 500000000;
+    address wirhdrawal_address = msg.sender;
     
     token_database db;
     
     function() payable {
-        if(block.timestamp > start_timestamp)
+        if(block.timestamp > end_timestamp || db.totalSupply() == max_cap)
         {
             throw;
         }
-        uint256 reward = GiveCoins_per_ETH.mul(msg.value) / 10**18;
+        
+        
+        uint256 reward = GiveCoins_per_ETH.mul( msg.value ) / 10**18;
+        if(db.totalSupply() + reward >= max_cap)
+        {
+            reward = max_cap - db.totalSupply();
+            msg.sender.send( msg.value.sub( reward.mul( 10**18 ) / GiveCoins_per_ETH ) );
+        }
         db.ICO_give_token(msg.sender, reward);
         Buy(msg.sender, msg.value, reward);
-    }
-    
-    function extendICO(uint256 _extension_period) only_owner
-    {
-        end_timestamp += _extension_period;
+        
     }
     
     function closeICO() only_owner
@@ -36,16 +41,31 @@ contract ICO is ownable{
         suicide(owner);
     }
     
+     /** DEBUGGING FUNCTIONS **/
+    
     function withdraw() only_owner
     {
         owner.send(this.balance);
     }
-    
-     /** DEBUGGING FUNCTIONS **/
      
     function adjust_price(uint256 _new_price) only_owner
     {
         GiveCoins_per_ETH = _new_price;
+    }
+    
+    function change_end_timestamp(uint256 _end_timestamp) only_owner
+    {
+        end_timestamp = _end_timestamp;
+    }
+    
+    function change_max_cap(uint256 _max_cap) only_owner
+    {
+        max_cap = _max_cap;
+    }
+    
+    function change_wirhdrawal_address(address _wirhdrawal_address) only_owner
+    {
+        wirhdrawal_address = _wirhdrawal_address;
     }
      
     function configure(address _token_database, uint _start_timestamp) only_owner
