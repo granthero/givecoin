f0d7287cd480f2b91cf079173299262d340b53dc
Dexaran <dexaran820@gmail.com>
2017-08-27 14:09:42 -0400

    Update token_database.sol

--- token_database.sol
+++ token_database.sol
@@ -1,100 +1,64 @@
 pragma solidity ^0.4.11;
 
-import './token_database.sol';
 import './ownable.sol';
 import './SafeMath.sol';
 
-contract ERC223ReceivingContract
-{
-    function tokenFallback(address, uint256, bytes) {}
-}
-
-contract token is ownable {
+contract token_database is ownable {
     using SafeMath for uint;
     
-    token_database public db;
+    address public token_contract;
     
-    event Transfer(address indexed from, address indexed to, uint indexed value, bytes data);
-    event Donation(string _donor, string recipient);
-    event Burn(address indexed _burner, uint256 indexed _amount);
-
-    function transfer(address _to, uint _value, bytes _data)
+    mapping(address => uint) public balances;
+    
+    uint256 public total_supply;
+    
+    string public name = "Test GiveCoin";
+    string public symbol = "Test GC";
+    uint8 public decimals = 2;
+    
+    function name() constant returns (string) { return name; }
+    function symbol() constant returns (string) { return symbol; }
+    function decimals() constant returns (uint8) {return decimals;}
+    
+    function token_database()
     {
-        uint codeLength;
-        assembly {
-            codeLength := extcodesize(_to)
-        }
-        
-        db.increase_balance(_to, _value);
-        db.decrease_balance(msg.sender, _value);
-        
-        if(codeLength>0) {
-            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
-            receiver.tokenFallback(msg.sender, _value, _data);
-        }
-        Transfer(msg.sender, _to, _value, _data);
+        balances[msg.sender] = 50000000 * 10**decimals;
+        total_supply = balances[msg.sender];
     }
     
-    function transfer(address _to, uint _value)
+    function totalSupply() constant returns (uint256 _supply)
     {
-        uint codeLength;
-        bytes memory _empty;
-
-        assembly {
-            codeLength := extcodesize(_to)
-        }
-        
-        db.increase_balance(_to, _value);
-        db.decrease_balance(msg.sender, _value);
-        
-        if(codeLength>0) {
-            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
-            
-            receiver.tokenFallback(msg.sender, _value, _empty);
-        }
-        Transfer(msg.sender, _to, _value, _empty);
-    }
-    
-    function donate(address _to, uint _value, bytes _data, string _donor, string _recipient)
-    {
-        transfer(_to, _value, _data);
-        Donation(_donor, _recipient);
+        return total_supply;
     }
 
     function balanceOf(address _owner) constant returns (uint _balance)
     {
-        return db.balanceOf(_owner);
+        return balances[_owner];
     }
-
-    function totalSupply() constant returns (uint _supply)
+    
+    function increase_balance(address _owner, uint256 _amount) only_token_contract
     {
-        return db.totalSupply();
+        balances[_owner] = balances[_owner].add(_amount);
     }
     
-    function name() constant returns (string)
+    function decrease_balance(address _owner, uint256 _amount) only_token_contract
     {
-        // Hardcoded value because of there is no possibility of returning
-        // string variables from `token_database` contract.
-        return "Test GiveCoin";
+        balances[_owner] = balances[_owner].sub(_amount);
     }
     
-    function symbol() constant returns (string)
-    {
-        // Hardcoded value because of there is no possibility of returning
-        // string variables from `token_database` contract.
-        return "Test GC";
-    }
-    
-    function decimals() constant returns (uint8)
-    {
-        return db.decimals();
-    }
-    
-    
-    /** DEBUGGING FUNCTIONS **/
+     /** DEBUGGING FUNCTIONS **/
      
-    function configure(address _state_storage) only_owner
+    function configure(address _ICO_contract, address _token_contract) only_owner
     {
-        db = token_database(_state_storage);
+        token_contract = _token_contract;
+    }
+    
+    modifier only_token_contract
+    {
+        if(msg.sender != token_contract)
+        {
+            throw;
+        }
+        _;
     }
 }
