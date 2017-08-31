cf00fde8579a7d310e5f071ac59f852032e78d19
Dexaran <dexaran820@gmail.com>
2017-08-31 04:55:29 -0400

    Comments added

--- ICO.sol
+++ ICO.sol
@@ -5,6 +5,10 @@ import './ownable.sol';
 import './token_database.sol';
 import './token.sol';
 
+/**
+ * @dev Provides a default implementation of an ICO contract that will be used to sell the specified amount of tokens
+ * at the given price.
+ */
 contract ICO is ownable{
     using SafeMath for uint;
     
@@ -12,15 +16,20 @@ contract ICO is ownable{
     
     uint256 public start_timestamp = now;
     uint256 public end_timestamp = now + 28 days;
-    uint256 public GiveCoins_per_ETH = 30000; // This means that 300 GC per 1 ETH
+    uint256 public GiveCoins_per_ETH = 30000; // This means 300 GC per 1 ETH
     address public wirhdrawal_address = msg.sender;
     
     mapping (address => bool) muted;
     
     token public GiveCoin_token;
     
-    // Mute sender to prevent it from calling function recursively
+     /**
+     * @dev Fallback function that will be called
+     *      whenever someone wants to purchase tokens from the ICO
+     */
     function() payable mutex(msg.sender) {
+    // Mute sender to prevent it from calling function recursively
+    
         if(block.timestamp > end_timestamp || block.timestamp < start_timestamp || msg.value < 10000000000000000)
         {
             throw;
@@ -41,11 +50,21 @@ contract ICO is ownable{
         
     }
     
+     /**
+     * @dev ERC223 standard `tokenFallback` function to handle incoming token transactions.
+     * @param _addr   The address of the contract of the tokens that have been deposited.
+     * @param _amount The amount of the tokens that have been deposited.
+     * @param _data   Additional transaction data.
+     */
     function tokenFallback(address _addr, uint256 _amount, bytes _data)
     {
         require(msg.sender == address(GiveCoin_token));
     }
     
+    
+     /**
+     * @dev A function to suicide contract after the end of ICO.
+     */
     function closeICO() only_owner
     {
         suicide(owner);
@@ -53,31 +72,64 @@ contract ICO is ownable{
     
      /** DEBUGGING FUNCTIONS **/
     
+    
+     /**
+     * @dev Debugging function that allows owner to withdraw funds from the contract.
+     */
     function withdraw() only_owner
     {
         owner.send(this.balance);
     }
-     
+    
+     /**
+     * @dev Debugging function that allows owner to adjust Give token price
+     *      due to the high volatility of ETH. 
+     * @param _new_price The amount of Give tokens that would be granted to
+     *        contributor for each 10**18 WEI.
+     *        _new_price should be equal to ETH/USD * 100 to set Give token
+     *        price rate to 1 USD.
+     */
     function adjust_price(uint256 _new_price) only_owner
     {
         GiveCoins_per_ETH = _new_price;
     }
     
+     /**
+     * @dev Debugging function that allows owner to set the end timestamp for the ICO.
+     * @param _end_timestamp New timestamp when the ICO will close.
+     */
     function change_end_timestamp(uint256 _end_timestamp) only_owner
     {
         end_timestamp = _end_timestamp;
     }
     
+     /**
+     * @dev Debugging function that allows owner to withdraw the specified amount
+     *      of tokens from the ICO contract.
+     * @param _amount Amount of tokens to withdraw.
+     */
     function withdraw_tokens(uint256 _amount) only_owner
     {
         GiveCoin_token.transfer(owner, _amount);
     }
     
+     /**
+     * @dev Debugging function that allows owner to set the withdrawal address.
+     * @param _wirhdrawal_address ETH will be sent to this address after purchasing tokens
+     *        from the ICO contract.
+     */
     function change_wirhdrawal_address(address _wirhdrawal_address) only_owner
     {
         wirhdrawal_address = _wirhdrawal_address;
     }
-     
+    
+     /**
+     * @dev Debugging function that allows owner to connect the ICO contract
+     *      with Give Token contract and set the start and end timestamps.
+     * @param _token_contract  Address of Give Token contract.
+     * @param _start_timestamp New timestamp when the ICO will start.
+     * @param _end_timestamp   New timestamp when the ICO will close.
+     */
     function configure(address _token_contract, uint _start_timestamp, uint _end_timestamp) only_owner
     {
         GiveCoin_token = token(_token_contract);
